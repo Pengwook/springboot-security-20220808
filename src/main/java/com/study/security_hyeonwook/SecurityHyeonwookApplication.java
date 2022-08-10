10e65e3f5b9b106228623fa0c403e6ee4367cfdf
LAPTOP-LJ90HDD8\khw12 <khw1204@naver.com>
2022-08-10 19:48:04 +0900

    시큐리티 시작

---
+++ src/main/java/com/study/security_hyeonwook/SecurityHyeonwookApplication.java
@@ -0,0 +1,13 @@
+package com.study.security_hyeonwook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHyeonwookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityHyeonwookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/security_hyeonwook/SecurityHyeonwookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.security_hyeonwook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityHyeonwookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
