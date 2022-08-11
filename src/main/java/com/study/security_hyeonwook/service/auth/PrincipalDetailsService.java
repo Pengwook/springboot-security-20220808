c55a2b1b12cae531b3e4fa38358637190213e8ae
LAPTOP-LJ90HDD8\khw12 <khw1204@naver.com>
2022-08-11 21:13:41 +0900

    메이플스토리 보지마세요

--- src/main/java/com/study/security_hyeonwook/service/auth/PrincipalDetailsService.java
+++ src/main/java/com/study/security_hyeonwook/service/auth/PrincipalDetailsService.java
@@ -2,11 +2,9 @@ package com.study.security_hyeonwook.service.auth;
 
 import java.util.Collection;
 
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.study.security_hyeonwook.domain.user.User;
@@ -35,24 +33,12 @@ public class PrincipalDetailsService implements UserDetailsService {
 			throw new UsernameNotFoundException(username + "사용자이름은 사용 할 수 없습니다.");
 		}
 		
-
-		public boolean addUser() {
-			User user = User.builder()
-					.user_name("길현욱")
-					.user_email("khw7219@gmail.com")
-					.user_id("abcd")
-					.user_password(new BCryptPasswordEncoder().encode("1234"))
-					.user_roles("ROLE_USER, ROLE_MANAGER")
-					.build();
-
-			try {
-				userRepository.save(user);
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-			return true;
+		return new PrincipalDetails(userEntity);
+	}
+	
+	public boolean addUser(SignupReqDto signupReqDto) throws Exception {
+		
+		return userRepository.save(signupReqDto.toEntity()) > 0;
 	}
 
 }
-
